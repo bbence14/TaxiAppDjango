4c09e3415276f1558e522488de9d9a7e2877ef5a
babicsbence <bbence14@gmail.com>
2023-01-08 19:42:15 +0100

    established websocket connection between client and server using JWT and middleware

--- server/taxi/asgi.py
+++ server/taxi/asgi.py
@@ -10,10 +10,19 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
+
+from taxi.middleware import TokenAuthMiddlewareStack
+from trips.consumer import TaxiConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taxi.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
+    'websocket': TokenAuthMiddlewareStack(
+        URLRouter([
+            path('taxi/', TaxiConsumer.as_asgi()),
+        ])
+    ),
 })
