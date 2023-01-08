7eab8adf2cbd7c4c4773565e01d9fae7f5e4fe98
babicsbence <bbence14@gmail.com>
2023-01-08 19:51:21 +0100

    Django secret changed

--- server/taxi/settings.py
+++ server/taxi/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yu7dxw0e9jurcdh-8il!g&*9*7kklh-$v=r)ta)1p*4+603$sc'
+SECRET_KEY = os.getenv('TAXI_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
